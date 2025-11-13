749388619c3d3d1ecf23d9840327b3d7d3c6bbb5
Miguel de Macedo <miguelmdemacedo@gmail.com>
2025-11-13 08:48:32 -0300

    muda o desafio

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <>
             <h1>Jeni, te amo! 😍</h1>
-            <p>Se você me ama, manda uma foto sua bebendo água</p>
+            <p>Me manda um coraçãozão! ❤️</p>
         </>
     )
 }
