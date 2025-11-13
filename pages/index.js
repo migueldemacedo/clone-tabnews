2023971c02b37e28de2a66ec2d85c8eee782e751
Miguel de Macedo <miguelmdemacedo@gmail.com>
2025-11-13 08:49:11 -0300

    corrige a frase do desafio

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <>
             <h1>Jeni, te amo! 😍</h1>
-            <p>Me manda um coraçãozão! ❤️</p>
+            <p>Se você também me ama, manda um coraçãozão! ❤️</p>
         </>
     )
 }
