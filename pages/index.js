d02908bcd073ec713cec3c6809043dd56197c781
Miguel de Macedo <miguelmdemacedo@gmail.com>
2025-11-12 23:36:56 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>Jeni, te amo! 😍</h1>
+            <p>Se você me ama, manda uma foto sua bebendo água 🫗</p>
+        </>
+    )
+}
+
+export default Home;
