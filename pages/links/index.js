c5daeab972794f244a51e56b1cc261c4b8cba98f
Miguel de Macedo <miguelmdemacedo@gmail.com>
2025-11-17 09:23:33 -0300

    adicao da secao de de links externos

---
+++ pages/links/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <a href="https://www.notion.so/Intelig-ncia-Artificial-Sem-Mist-rio-Turbinando-Sua-Produtividade-2a0d5b39e29a8038b9e3d4d348f82f6d?source=copy_link">
+      Notion
+    </a>
+  );
+}
+
+export default Home;
