e1060a022ad1b55c1db2679a56269ef937c80f5e
miguelmacedo <miguelmdemacedo@gmail.com>
2025-12-16 11:06:24 -0300

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+function Home() {
+    return (
+        <>
+            <h1>“Angelus”</h1>
+            <div>
+                <p>
+                    <strong>℣.</strong> O anjo do Senhor anunciou a Maria.
+                </p>
+                <p>
+                    <strong>℟.</strong> E ela concebeu do Espírito Santo.
+                </p>
+                <em>Ave-Maria.</em>
+            </div>
+            <br />
+            <div>
+                <p>
+                    <strong>℣.</strong> Eis aqui a escrava do Senhor.
+                </p>
+                <p>
+                    <strong>℟.</strong> Faça-se em mim segundo a vossa palavra.
+                </p>
+                <em>Ave-Maria.</em>
+            </div>
+            <br />
+            <div>
+                <p>
+                    <strong>℣.</strong> E o Verbo se fez carne.
+                </p>
+                <p>
+                    <strong>℟.</strong> E habitou entre nós.
+                </p>
+                <em>Ave-Maria.</em>
+            </div>
+            <br />
+            <div>
+                <p>
+                    <strong>℣.</strong> Rogai por nós, santa Mãe de Deus.
+                </p>
+                <p>
+                    <strong>℟.</strong> Para que sejamos dignos das promessas de Cristo.
+                </p>
+            </div>
+            <br />
+            <div>
+                <p>
+                    <strong>Oremos</strong>
+                </p>
+                <p>
+                    Infundi, Senhor, nós vos pedimos, em nossas almas a vossa graça, para que nós, que conhecemos pela anunciação do Anjo a encarnação de Jesus Cristo, vosso Filho, cheguemos por sua paixão e sua cruz à glória da ressurreição. Pelo mesmo Cristo, Senhor nosso. Amém.
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Home;
